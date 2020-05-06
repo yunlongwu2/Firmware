207a6b79f2bbe95a5ea5edccec470b2ee48e9ee4
Daniel Agar <daniel@agar.ca>
2020-05-06 12:24:08 -0400

    mavlink_log_info: also print PX4_INFO
     - any information sent to the ground should also be available for log review

--- src/lib/systemlib/mavlink_log.h
+++ src/lib/systemlib/mavlink_log.h
@@ -79,7 +79,11 @@ __EXPORT void mavlink_vasprintf(int severity, orb_advert_t *mavlink_log_pub, con
  * @param _pub		Pointer to the uORB advert;
  * @param _text		The text to log;
  */
-#define mavlink_log_info(_pub, _text, ...)	mavlink_vasprintf(_MSG_PRIO_INFO, _pub, _text, ##__VA_ARGS__);
+#define mavlink_log_info(_pub, _text, ...) \
+	do { \
+		mavlink_vasprintf(_MSG_PRIO_INFO, _pub, _text, ##__VA_ARGS__); \
+		PX4_INFO(_text, ##__VA_ARGS__); \
+	} while(0);
 
 /**
  * Send a mavlink warning message and print to console.
